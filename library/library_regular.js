3ef13fceb6a48a609519d49a674aaadc231c125b
小境Area <areaxj@outlook.com>
2024-06-09 18:48:03 +0800

    library

---
+++ library/library_regular.js
@@ -0,0 +1,9 @@
+function text_print (str){
+	document.write (str);
+}
+function lib_sidebox_goodworks() {
+	text_print('<a target="_blank" href="115237465.html">《早知如此，一定当初》作者不详，时间不详，完结</a><br><a target="_blank" href="113925754.html">《韵狐之歌》作者不详，时间不详，完结</a><br><a target="_blank" href="110287534.html">《养一只小奶猫》作者不详，时间不详，连载</a><br>')
+}
+function lib_pabbs_LICENSE_pmcl() {
+	text_print('<a target="_blank" href="http://parea.great-site.net/parea/137/pmcl/">《云之境内容共享协议》</a>')
+}
