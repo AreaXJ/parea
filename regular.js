dae7648892ebfb9066c3bb4c2ff55633c396f3a9
小境Area <areaxj@outlook.com>
2024-06-15 10:32:17 +0800

    update style and pages

--- regular.js
+++ regular.js
@@ -12,6 +12,43 @@ $(function() {
 		}
 	})
 });
+var darkmode = 0;
+var greenmode = 0;
+$(function() {
+	$("#darkmode-chb").click(function() {
+		if (this.checked) {
+			$("#greenmode-chb").attr("checked",false);greenmode=0;
+			$("#darkmode-chb").attr("checked",true);darkmode=1;
+		} else {
+			$("#darkmode-chb").attr("checked",false);darkmode=0;
+		}
+		theme_select();
+	})
+	$("#greenmode-chb").click(function() {
+		alert("The theme \"Greenmode\" is unfinished!")
+		/*if (this.checked) {
+			$("#darkmode-chb").attr("checked",false);darkmode=0;
+			$("#greenmode-chb").attr("checked",true);greenmode=1;
+		} else {
+			$("#greenmode-chb").attr("checked",false);greenmode=0;
+		}
+		theme_select();*/
+	})
+})
+function theme_select() {
+	console.log("select theme");console.log("darkmode" + darkmode);console.log("greenmode" + greenmode);
+	if (darkmode==0 & greenmode==0) {$("body").addClass("colorfulmode");} else {$("body").removeClass("colorfulmode");}
+	if (darkmode==1) {//darkmode
+		$("body").addClass("darkmode");
+	} else {
+		$("body").removeClass("darkmode");
+	}
+	if (greenmode==1) {//greenmode
+		$("body").addClass("greenmode");
+	} else {
+		$("body").removeClass("greenmode");
+	}
+}
 var pa = {};
 function text_print (str){
 	document.write (str);
@@ -31,6 +68,9 @@ function connect_way (str){
 function footer_notice() {
 	text_print("愿为一片净土");
 }
+function sidebox_notice() {
+	text_print("愿为一片净土");
+}
 function footer_links() {
 	text_print("<a href='https://pixiv.net'>Pixiv</a><br><a href='https://www.writing.com'>writing.com</a><br><a href='http://parea.great-site.net'>云之境</a><br>");
 }
@@ -38,7 +78,7 @@ function sidebox_links() {
 	text_print("<p class='main-sidebox-section-text'><a href='https://pixiv.net'>Pixiv</a></p><p class='main-sidebox-section-text'><a href='https://www.writing.com'>writing.com</a></p><p class='main-sidebox-section-text'><a href='http://parea.great-site.net'>云之境</a></p>");
 }
 function copyright() {
-	text_print("Copyright © 2022-2023 云野.All Rights Reserved.<br>Copyright © 2022-2023 Pregantain All Group.All rights reserved.<br>云野2022-2023版权所有。保留所有权利。")
+	text_print("Copyright © 2022-2024 云野.All Rights Reserved.<br>Copyright © 2022-2024 Pregantain All.All rights reserved.<br>云野2022-2024版权所有。保留所有权利。")
 }
 //Library
 function lib_booksnum() {
