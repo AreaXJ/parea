ab4f4343ee13572069be764954cf774bfece0220
小境Area <areaxj@outlook.com>
2024-06-15 10:33:31 +0800

    update style and pages

--- library/library_regular.js
+++ library/library_regular.js
@@ -1,8 +1,25 @@
+
+$(function() {
+	$("#bookcard-chb").click(function() {
+		if (this.checked) {
+			$("#bookcard-chb").attr("checked",true);
+			$("body").addClass("bookcard-listmode");
+			$("body").removeClass("bookcard-gridmode");
+		} else {
+			$("#bookcard-chb").attr("checked",false);
+			$("body").addClass("bookcard-gridmode");
+			$("body").removeClass("bookcard-listmode");
+		}
+	})
+})
 function text_print (str){
 	document.write (str);
 }
+function lib_footer_notice() {
+	text_print("<br>云野图书馆，让每一个人都能自由地阅读！<br>欢迎提供更多文章！");
+}
 function lib_sidebox_goodworks() {
-	text_print('<a target="_blank" href="115237465.html">《早知如此，一定当初》作者不详，时间不详，完结</a><br><a target="_blank" href="113925754.html">《韵狐之歌》作者不详，时间不详，完结</a><br><a target="_blank" href="110287534.html">《养一只小奶猫》作者不详，时间不详，连载</a><br>')
+	text_print('<a target="_blank" href="514875312.html">《迷茫都市》兔子头，2020-02-03，完结</a><br><a target="_blank" href="115237465.html">《早知如此，一定当初》作者不详，时间不详，完结</a><br><a target="_blank" href="113925754.html">《韵狐之歌》作者不详，时间不详，完结</a><br><a target="_blank" href="114865473.html">《景阳冈武二娘打虎》巴黎圣母院敲钟，时间不详，完结</a><br>')
 }
 function lib_pabbs_LICENSE_pmcl() {
 	text_print('<a target="_blank" href="http://parea.great-site.net/parea/137/pmcl/">《云之境内容共享协议》</a>')
